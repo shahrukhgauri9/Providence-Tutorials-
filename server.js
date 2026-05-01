305092ec84f0184c0a66b671c8128193297e9dee
Shahrukh Gori <shahrukhgauri09@gmail.com>
2026-05-01 22:35:19 +0530

    server.js created
    Signed-off-by: Shahrukh Gori <shahrukhgauri09@gmail.com>

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+// Simple test route
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Backend is working!' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
